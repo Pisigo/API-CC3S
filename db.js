6eeb94582ef44ea713c87c084d60449dc0300650
Gabriel Victor <erivaldo.gabriel9@gmail.com>
2025-05-13 12:02:22 -0300

    depuração de erros

--- db.js
+++ db.js
@@ -1,25 +1,31 @@
-  import mysql from 'mysql2';
-  import dotenv from 'dotenv';
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
 
-  dotenv.config();
+dotenv.config();
 
-  const conexao = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 3306,
-    ssl: {
-    rejectUnauthorized: true
-    }
-  });
+const conexao = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+  rejectUnauthorized: true
+  }
+});
 
-  conexao.connect(err => {
-    if (err) {
-      console.error('Erro ao conectar no banco:', err);
-    } else {
-      console.log('Conectado ao MySQL!');
-    }
-  });
+console.log('Variáveis de ambiente:');
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_PORT:', process.env.DB_PORT);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_NAME:', process.env.DB_NAME);
+conexao.connect(err => {
+  if (err) {
+    console.error('Erro ao conectar no banco:', err);
+  } else {
+    console.log('Conectado ao MySQL!');
+  }
+});
 
-  export default conexao;
+export default conexao;
