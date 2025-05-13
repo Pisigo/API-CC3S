4cc043c0b49f8d9396513f9d6c5863164c4c1911
Gabriel Victor <erivaldo.gabriel9@gmail.com>
2025-05-13 11:56:14 -0300

    mudando tipo coneção ssl

--- db.js
+++ db.js
@@ -8,7 +8,10 @@
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT 
+    port: process.env.DB_PORT || 3306,
+    ssl: {
+    rejectUnauthorized: true
+    }
   });
 
   conexao.connect(err => {
